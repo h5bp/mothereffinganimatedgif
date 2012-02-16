d02f27741cde3fb8564d46638148f0bdf611eaed
Carson McDonald <carson@ioncannon.net>
2012-02-16 17:20:01 -0500

    Add download link.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,10 +32,24 @@
             return 'data:image/gif;base64,' + $.base64.encode(encoder.stream().getData());
         };
 
+        var _binaryURL = function() {
+            // Convert encoder data to binary format
+            var data = encoder.stream().getData();
+            var byteArray = new Uint8Array(data.length);
+            for (var i = 0; i < data.length; i++) {
+                byteArray[i] = data.charCodeAt(i) & 0xff;
+            }
+
+            var bb = new BlobBuilder();
+            bb.append(byteArray.buffer);
+            return window.URL.createObjectURL(bb.getBlob("image/gif"));
+        };
+
         _initialize(images, delay, repeat, width, height);
 
         return {
-            dataURL: _dataURL
+            dataURL: _dataURL,
+            binaryURL: _binaryURL
         };
     }
 
@@ -86,12 +100,75 @@
         FileReaderJS.setupDrop(document.body, opts);
         FileReaderJS.setupClipboard(document.body, opts);
 
+        Modernizr.addTest({
+            // BlobBuilder
+            "blobbuilder": function() {
+                return  !!(window.BlobBuilder || window.MozBlobBuilder || window.WebKitBlobBuilder);
+            },
+            // Blob URL support
+            "bloburls": function() {
+                return !!(window.URL && window.URL.createObjectURL || window.webkitURL && window.webkitURL.createObjectURL);;
+            },
+            // download attribute
+            "download": function() {
+                return "download" in document.createElement("a");
+            },
+            // FormData
+            "formdata": function() {
+                return  !!(window.FormData && "append" in window.FormData.prototype);
+            }
+        });
+
+        if(!Modernizr.download && $('#saveabro')) {
+            var iframe = document.createElement("iframe");
+
+            iframe.src = "http://saveasbro.com/";
+            iframe.setAttribute("style","position: absolute; visibility: hidden; left: -999em;");
+            iframe.id = "saveasbro";
+            document.body.appendChild(iframe);
+        }
     });
 
     $('.play').on('click', function(e) {
         var mfAnimatedGIF = new MFAnimatedGIF(App.timeline, 300, false, 75, 75);
 
         $('#animresult').attr('src', mfAnimatedGIF.dataURL());
+
+        //
+        // Create the download link
+        //
+        window.URL = window.webkitURL || window.URL;
+        window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
+
+        var filename = "animated.gif";
+
+        var a = document.querySelector('#downloadlink');
+        a.style.display = "none";
+
+        if(Modernizr.download && Modernizr.bloburls && Modernizr.blobbuilder) {
+            a.download = filename;
+            a.href = mfAnimatedGIF.binaryURL(filename);
+            a.style.display = "inline-block";
+
+            a.onclick = function(e) {
+              // Need a small delay for the revokeObjectURL to work properly.
+              setTimeout(function() {
+                window.URL.revokeObjectURL(a.href);
+              }, 1500);
+            };
+        } else {
+            window.onmessage = function(e) {
+                e = e || window.event;
+
+                var origin = e.origin || e.domain || e.uri;
+                if(origin !== "http://saveasbro.com") return;
+                a.href = "http://saveasbro.com/download/" + e.data;
+                a.style.display = "inline-block";
+            };
+
+            var iframe = document.querySelector('#saveasbro');
+            iframe.contentWindow.postMessage(JSON.stringify({name:filename, data: mfAnimatedGIF.dataURL(), formdata: Modernizr.formdata}),"http://saveasbro.com");
+        }
 
         return false;
     });
