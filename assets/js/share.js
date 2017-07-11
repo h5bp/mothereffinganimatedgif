a3eaac1c420cf3c1fe5259c12806d3b709a342cd
Paul Irish <paul.irish@gmail.com>
2017-07-10 18:15:57 -0700

    start updating imgur API but quit because its hard and hide the button.

--- assets/js/share.js
+++ assets/js/share.js
@@ -2,23 +2,19 @@ var ShareGIFWith = function() {};
 
 ShareGIFWith.imgur = function(imageData, imageName, imageTitle, imageCaption, success, error) {
   $.ajax({
-      url: 'http://api.imgur.com/2/upload.json',
+      url: 'https://api.imgur.com/3/image',
       type: 'POST',
       data: {
           type   : 'base64',
           key    : 'b0b751fa5073aed697e0919602ce506d',
           name   : imageName,
           title  : imageTitle,
-          caption: imageCaption,
+          description: imageCaption,
           image  : imageData
       },
       dataType: 'json'
   }).success(function(data) {
-      success(data['upload']['links']['delete_page'],
-              data['upload']['links']['imgur_page'],
-              data['upload']['links']['large_thumbnail'],
-              data['upload']['links']['original'],
-              data['upload']['links']['small_square']);
+      success(data);
   }).error(function() {
       error();
   });
